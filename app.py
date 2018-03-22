9a4dbd17ee6e656623274a56c6e784bdc482c7cc
mzg <15122302865@163.com>
2018-03-23 07:15:11 +0800

    读取json（未完成）

--- app.py
+++ app.py
@@ -2,16 +2,26 @@
 from flask import Flask
 from flask import render_template
 
+import json
+import os
+
 app=Flask(__name__)
+path = '~/files'
 
 @app.route('/')
-def index():
+def index(title_mod):
     '''
     显示文章名称的列表
     页面中需要显示 ｀/home/shiyanlou/files/｀目录下所有的json文件
     中的｀title｀信息列表
     '''
-    return render_template('',)
+    files = os.listdir(path)
+    titles=[]
+    for file in files:
+        with open(path+'/'+file,'r') as f:
+            json_info = json.load(f)
+            titles.append(json_info['title'])
+    return render_template('index.html',title_mod=titles)
 
 
 @app.route('/files/<filename>')
