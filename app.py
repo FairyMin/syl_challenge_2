999e809483828e9ef99a3dceec87db3c04b509f8
mzg <15122302865@163.com>
2018-03-24 08:21:36 +0800

    添加ORM映射类

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 # -*- coding=utf-8 -*-
 from flask import Flask
 from flask import render_template,abort
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 import json
 import os
 
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root@localhost/mydb'
+db=SQLAlchemy(app)
+
 #读取文件中的文件名
 path = '/home/shiyanlou/files'
 files = os.listdir(path)
 
+#首页视图函数
 @app.route('/')
 def index():
     '''
@@ -26,7 +32,7 @@ def index():
     #返回模板并渲染
     return render_template('index.html',title_mod=titles)
 
-
+#files页视图函数
 @app.route('/files/<filename>')
 def file(filename):
     '''
@@ -50,9 +56,56 @@ def file(filename):
     #返回模板并渲染
     return render_template('file.html',file_info_mod=file_info_dic)
 
+#404视图函数
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'),404
+
+
+#文章表，类别表的关系是 '多对一' 的关系，即一个类别有多篇文章
+
+#数据表 File类－－对应－－文章表
+class File(db.Model):
+    #主键
+    id = db.Column(db.Integer,primary_key=True)
+    #文章标题
+    title = db.Column(db.String(80))
+    #文章创建时间
+    created_time = db.Column(db.DateTime)
+    #文章分类，外键约束
+    category_id = db.Column(db.Integer,db.ForeignKey('category.id'))
+    category = db.relationship('Category',backref=db.backref('files',
+                lazy='dynamic'))
+    #文章内容
+    content = db.Column(db.Text)
+
+    def __init__(self,title,category,content,created_time=None):
+        self.title = title
+        
+        if created_time is None:
+            created_time = datetime.now()
+        self.created_time = created_time
+
+        self.category = category
+        self.content = content
+
+
+    def __repr__(self):
+        return '<File %s>'%self.title
+
+
+#数据表 Category类－－对应－－类别表
+class Category(db.Model):
+    #主键id
+    id = db.Column(db.Integer,primary_key=True)
+    #类别名称
+    name = db.Column(db.String(80))
+    
+    def __init__(self,name):
+        self.name = name
+
+    def __repr__(self):
+        return '<Category %s>'%self.name
 
 
 if __name__=="__main__":
